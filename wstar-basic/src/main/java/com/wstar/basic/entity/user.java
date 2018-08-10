85559bba60798ffea71406246202ab10833f9eac
zhangyao <zhangyao@chudaokeji.com>
2018-08-10 15:59:04 +0800

    init

---
+++ wstar-basic/src/main/java/com/wstar/basic/entity/user.java
@@ -0,0 +1,5 @@
+package com.wstar.basic.entity;
+
+public class user {
+
+}
---
+++ wstar-basic/src/main/java/com/wstar/basic/enums/ResultStatus.java
@@ -0,0 +1,17 @@
+package com.wstar.basic.enums;
+
+public enum ResultStatus {
+
+	/**
+	 * 成功
+	 */
+	success,
+	/**
+	 * 参数错误
+	 */
+	paramError,
+	/**
+	 * 系统错误
+	 */
+	sysError
+}
